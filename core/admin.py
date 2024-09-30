72e9e4e2472a97749f8d59453f455e2d56ff6f18
Adriano Santos <adrianosantosdi@gmail.com>
2024-09-30 12:13:41 -0300

    Criado os models Pessoa, Marca, Veiculos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Pessoa, Marca, Veiculo
 
-# Register your models here.
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'telefone', 'endereco')
+
+
+@admin.register(Marca)
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+
+@admin.register(Veiculo)
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ('marca', 'placa', 'cor', 'observacoes')    
